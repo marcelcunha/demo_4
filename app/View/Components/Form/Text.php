8947071d3f8ca4b2942cb1954d858aba9a8908a3
Marcel Cunha <marcel.cunha.mc@gmail.com>
2023-09-19 07:04:06 -0300

    feat: cria components de input

---
+++ app/View/Components/Form/Text.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\View\Components\Form;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class Text extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public function __construct(public string $name, public string $label, public ?string $id = null, public string $type = 'text')
+    {
+        if (is_null($id)) {
+            $this->id = $name;
+        }
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.form.text');
+    }
+}
