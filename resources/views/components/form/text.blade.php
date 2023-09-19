782034b0926d22a4b74b7dcfe756c1dad5853ea7
Marcel Cunha <marcel.cunha.mc@gmail.com>
2023-09-19 09:51:56 -0300

    feat: atualiza component text input

--- resources/views/components/form/text.blade.php
+++ resources/views/components/form/text.blade.php
@@ -5,7 +5,9 @@
 
         <input type="{{ $type }}" name="{{ $name }}" id="{{ $id }}" @class([
              'ring-red-300 placeholder:text-red-300 focus:ring-red-500' => $errors->has($name) ,
-            'block w-full rounded-md border-0 py-1.5 px-2  ring-1 ring-inset  focus:ring-2 focus:ring-inset  sm:text-sm sm:leading-6',
+            'block w-full rounded-md border-0 py-1.5 px-2  ring-1 ring-inset  focus:ring-2 focus:ring-inset  sm:text-sm sm:leading-6
+            disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500 disabled:ring-gray-200
+             read-only:bg-gray-50 read-only:text-gray-500 read-only:ring-gray-200',
         ])
             {{ $attributes }}>
 
@@ -22,6 +24,6 @@
     </div>
 
     @error($name)
-        <p class="mt-2 text-sm text-red-600" id="email-error">{{ $message }}</p>
+        <p class="mt-2 text-sm text-red-600 " id="email-error">{{ $message }}</p>
     @enderror
 </div>
