7ff82f4574f9d17ed3568a09960fd3d63586865e
Marcel Cunha <marcel.cunha.mc@gmail.com>
2023-09-19 09:51:19 -0300

    feat: atualiza componente textaread

--- resources/views/components/form/text-area.blade.php
+++ resources/views/components/form/text-area.blade.php
@@ -1,13 +1,17 @@
 <div>
-    <label for="{{$id}}" class="block text-sm font-medium leading-6 text-gray-900">{{ $label }}</label>
+    <label for="{{ $id }}" class="block text-sm font-medium leading-6 text-gray-900">{{ $label }}</label>
 
     <div class="relative mt-2 rounded-md shadow-sm">
 
-        <textarea  name="{{ $name }}" id="{{ $id }}" {{$attributes->class([
-             'ring-red-300 placeholder:text-red-300 focus:ring-red-500' => $errors->has($name) ,
-            'block w-full rounded-md border-0 py-1.5 px-2  ring-1 ring-inset  focus:ring-2 focus:ring-inset  sm:text-sm sm:leading-6',
-        ])}}
+        <textarea name="{{ $name }}" id="{{ $id }}"
+            {{ $attributes->class([
+                'ring-red-300 placeholder:text-red-300 focus:ring-red-500' => $errors->has($name),
+                'block w-full rounded-md border-0 py-1.5 px-2  ring-1 ring-inset  focus:ring-2 focus:ring-inset  sm:text-sm sm:leading-6
+                        disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500 disabled:ring-gray-200
+                        read-only:bg-gray-50 read-only:text-gray-500 read-only:ring-gray-200',
+            ]) }}
             {{ $attributes }}>
+            {{ $slot }}
         </textarea>
 
 
