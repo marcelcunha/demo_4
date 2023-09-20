3b026e823cdb35bbe5a16952b2709e077687d3a0
Marcel Cunha <marcel.cunha.mc@gmail.com>
2023-09-20 12:13:41 -0300

    feat: cria serviço para acesso a api de clima

---
+++ app/Services/WeatherService.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Http;
+
+class WeatherService
+{
+    private string $apiKey;
+
+    private string $apiUrl;
+
+    public function __construct()
+    {
+        $this->apiKey = config('weather.api_key');
+        $this->apiUrl = config('weather.api_url');
+    }
+
+    public function getWeather(): array
+    {
+        $response = $this->getCachedResponse();
+
+        if (empty($response)) {
+            return $response;
+        }
+
+        return [
+            'temp' => $response['results']['temp'],
+            'date' => $response['results']['date'],
+            'time' => $response['results']['time'],
+            'description' => $response['results']['description'],
+            'currently' => $response['results']['currently'],
+            'city' => $response['results']['city'],
+            'cloudiness' => $response['results']['cloudiness'],
+            'condition_slug' => $response['results']['condition_slug'],
+        ];
+    }
+
+    private function callAPI(): array
+    {
+        return Http::acceptJson()
+            ->get($this->apiUrl, [
+                'key' => $this->apiKey,
+                'user_ip' => 'remote',
+            ])->throw(function ($response) {
+                return [];
+            })
+            ->json();
+    }
+
+    private function getCachedResponse(): array
+    {
+        return Cache::remember('weather', 3600, function () {
+            return $this->callAPI();
+        });
+    }
+}
---
+++ config/weather.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'api_key' => env('WEATHER_API_KEY'),
+    'api_url' => env('WEATHER_API_URL'),
+];
