4d81fe5f7d306de074b19e7c5cd4636f5ae93da6
Marcel Cunha <marcel.cunha.mc@gmail.com>
2023-09-20 19:20:11 -0300

    test: adiciona teste de deleção

--- tests/Feature/Book/IndexTest.php
+++ tests/Feature/Book/IndexTest.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Livewire\Book\Index;
+use App\Models\Book;
 use App\Models\User;
+use Livewire\Livewire;
 
 test('book list cant be showed if not authenticated', function () {
     $response = $this->get(route('books.index'));
@@ -14,4 +17,17 @@ it('should display book list if authenticated', function () {
         ->get(route('books.index'));
 
     $response->assertStatus(200)->assertSee('Listagem de Livros');
+});
+
+it('should delete book if call remove method', function(){
+    $book = Book::factory()->create();
+
+   Livewire::actingAs(User::factory()->create())
+        ->test(Index::class)
+        ->call('remove', $book->id)
+        ->assertRedirect(route('books.index'));
+
+    $this->assertDatabaseMissing('books', [
+        'id' => $book->id,
+    ]);
 });
